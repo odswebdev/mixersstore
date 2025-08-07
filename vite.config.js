5cb2370e4254ff887a862de9e284db45fe1125af
Dmitry Orlov <ods90@mail.ru>
2025-08-08 03:23:15 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: mode === "development" ? "/" : "/mixersstore/",
+  base: "/mixersstore/",
   build: {
     chunkSizeWarningLimit: 1000, // или любое другое значение в KB
   },
